71729a82eb22c9e9e7d4b7e79a7c01c11fbeb9bf
sevakul16 <sevakul16@gmail.com>
2024-12-09 16:52:16 +0200

    Added authorizing

---
+++ src/app/(dashboard)/chat/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChatPage = () => {
+  return <div>cat</div>;
+};
+
+export default ChatPage;
---
+++ src/app/sign-up/[[...sign-up]]/page.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { SignUp } from "@clerk/nextjs";
+
+const SignUpPage = () => {
+  <div className="min-h-screen flex justify-center items-center">
+    <SignUp />
+  </div>;
+};
+
+export default SignUpPage;
